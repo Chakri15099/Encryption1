b50553a2d4df64e8467ed4d7637ce17c1906245f
kundanaSakhamuri98 <s545248@nwmissouri.edu>
2022-03-19 00:06:21 -0500

    Added navigation from encryption to SendActivity page. Current commit and last commit are done by Shruthi Patkur(used Kundana Sakhamuri's github account as I faced few last minute login issues with my account)

--- app/src/main/java/com/example/encryption1/EncryptActivity.java
+++ app/src/main/java/com/example/encryption1/EncryptActivity.java
@@ -2,13 +2,29 @@ package com.example.encryption1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EncryptActivity extends AppCompatActivity {
 
+    Button nav_new_message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypt);
+
+        nav_new_message = findViewById(R.id.sendBTN);
+
+
+        nav_new_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent  (EncryptActivity.this,SendActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
