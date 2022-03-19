505e0f548b35d6c09124e45563b0354b8cea532a
Chakri15099 <chakradhar.prathivada1509@gmail.com>
2022-03-18 21:23:54 -0500

    Created a project and added MainActivity, LoginActivity, RegistrationActivity along with corresponding XML files. Modified manifest and strings.xml

---
+++ app/src/main/java/com/example/encryption1/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.encryption1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button nav_register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        nav_register = findViewById(R.id.createBTN);
+
+        nav_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent (LoginActivity.this,RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/encryption1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.encryption1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img = findViewById(R.id.img);
+        img.setAlpha(0.0f);
+        img.animate().alpha(1.0f).setDuration(5000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        },7800);
+    }
+    }
