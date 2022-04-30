da691667eb86388f9f84ed566677968c44763f96
Chakri15099 <chakradhar.prathivada1509@gmail.com>
2022-04-30 18:52:08 -0500

    importimg requires packages in HomeActivity.java file

--- app/src/main/java/com/example/encryption1/HomeActivity.java
+++ app/src/main/java/com/example/encryption1/HomeActivity.java
@@ -12,9 +12,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
