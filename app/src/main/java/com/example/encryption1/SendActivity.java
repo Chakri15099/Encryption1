33a1530307302af8a75128d2e08e5df0033134fb
Chakri15099 <chakradhar.prathivada1509@gmail.com>
2022-04-30 19:03:10 -0500

    Imported required packages in SendActivity.java

--- app/src/main/java/com/example/encryption1/SendActivity.java
+++ app/src/main/java/com/example/encryption1/SendActivity.java
@@ -9,9 +9,22 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SendActivity extends AppCompatActivity {
     FirebaseAuth auth1;
