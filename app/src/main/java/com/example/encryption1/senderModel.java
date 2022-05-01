b9ef732e021c090a4dcc69c076a52a8ac136d192
pshruthi04 <89540632+pshruthi04@users.noreply.github.com>
2022-04-30 19:23:31 -0500

    added getter and setter for both the variables in senderModel.java

--- app/src/main/java/com/example/encryption1/senderModel.java
+++ app/src/main/java/com/example/encryption1/senderModel.java
@@ -11,6 +11,20 @@ public class senderModel {
 
     public senderModel() {
     }
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
 
 }
